b16fdbb650fbe46348e6ceb731a19d68f74ea159
Maja <maya.grudge.12@gmail.com>
2022-11-29 19:14:49 +0100

    Initialized spring boot project. Added neccessary dependencies.

---
+++ transport_projekat_backend/src/test/java/ftn/uns/ac/rs/transport/MainTests.java
@@ -0,0 +1,13 @@
+package ftn.uns.ac.rs.transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
