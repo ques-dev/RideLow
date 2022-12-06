48801376d01fc8297990c340ef031e0c67be43fd
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2022-12-06 20:33:26 +0100

    Add GET /api/driver/{id}/ride endpoint
    Add corresponding methods to Ride repository, service, DriverController.
    Added some dummy data to the database via data.sql.

--- transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/repository/RideRepository.java
+++ transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/repository/RideRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rs.ac.uns.ftn.transport.model.Ride;
 
 import java.time.LocalDateTime;
@@ -16,7 +17,8 @@ public interface RideRepository extends JpaRepository<Ride,Integer> {
     @Query("SELECT r FROM Ride r LEFT JOIN r.passengers pass " +
             "WHERE pass.id = ?1 AND r.startTime >= ?2 AND r.endTime <= ?3")
     Page<Ride> findRidesBetweenDateRange(Integer passengerId, LocalDateTime start, LocalDateTime end, Pageable page);
-
+    Page<Ride> findAllByDriver_Id(@Param("id") Integer id, Pageable page);
+    Page<Ride> findAllByDriver_IdAndStartTimeIsAfterAndEndTimeIsBefore(@Param("id") Integer id, @Param("from") LocalDateTime start, @Param("to") LocalDateTime end, Pageable page);
+    Page<Ride> findAllByDriver_IdAndStartTimeIsAfter(@Param("id") Integer id, @Param("from") LocalDateTime start, Pageable page);
+    Page<Ride> findAllByDriver_IdAndEndTimeIsBefore(@Param("id") Integer id, @Param("to") LocalDateTime end, Pageable page);
 }
-
-
--- transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/service/RideService.java
+++ transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/service/RideService.java
@@ -1,5 +1,7 @@
 package rs.ac.uns.ftn.transport.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.transport.model.Driver;
 import rs.ac.uns.ftn.transport.model.Rejection;
@@ -35,5 +37,21 @@ public class RideService implements IRideService {
         ride.setTotalCost(1234.0);
         ride.setStatus(RideStatus.PENDING);
         return rideRepository.save(ride);
+    }
+
+    public Page<Ride> findAllByDriver_Id(Integer id, Pageable page) {
+        return rideRepository.findAllByDriver_Id(id, page);
+    }
+
+    public Page<Ride> findAllByDriver_IdAndStartTimeIsAfterAndEndTimeIsBefore(Integer id, LocalDateTime start, LocalDateTime end, Pageable page) {
+        return rideRepository.findAllByDriver_IdAndStartTimeIsAfterAndEndTimeIsBefore(id, start, end, page);
+    }
+
+    public Page<Ride> findAllByDriver_IdAndStartTimeIsAfter(Integer id, LocalDateTime start, Pageable page) {
+        return rideRepository.findAllByDriver_IdAndStartTimeIsAfter(id, start, page);
+    }
+
+    public Page<Ride> findAllByDriver_IdAndEndTimeIsBefore(Integer id, LocalDateTime end, Pageable page) {
+        return rideRepository.findAllByDriver_IdAndEndTimeIsBefore(id, end, page);
     }
 }
