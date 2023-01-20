daba56ab6f1c8eb2e41216a2ac19bbbba51ede7c
dracooya <maya.grudge.12@gmail.com>
2023-01-20 15:05:14 +0100

    Slight fix with socket configuration.

--- transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/config/WebSocketConfiguration.java
+++ transport_projekat_backend/src/main/java/rs/ac/uns/ftn/transport/config/WebSocketConfiguration.java
@@ -13,7 +13,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket").withSockJS();
+        registry.addEndpoint("/socket").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
